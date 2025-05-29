e4367d034ec9a15a54ea51cd596df6c271755b41
Thuy Le <ngocthuyle19@gmail.com>
2025-05-29 12:01:24 -0400

    create contact

--- client/src/components/Contacts.tsx
+++ client/src/components/Contacts.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+
+import { useContacts } from "../contexts/ContactsContext";
+import type { ContactProps } from "../type";
 
 export default function Contacts() {
+  const { contacts } = useContacts() as {contacts: ContactProps[]};
+  console.log("contacts", contacts);
   return (
-    <div>
-      
+    <div className="mt-10">
+      {contacts.map((contact) => (
+        <ul className="text-left mt-2" key={contact.id}>{contact.name}</ul>
+      ))}
     </div>
-  )
+  );
 }
--- client/src/components/NewContactModal.tsx
+++ client/src/components/NewContactModal.tsx
@@ -1,18 +1,29 @@
+import { useForm } from "react-hook-form";
+import type { ContactProps, createContactProps } from "../type";
+import { useContacts } from "../contexts/ContactsContext";
 
 export default function NewContactModal({
   closeModal,
 }: {
   closeModal: () => void;
 }) {
+  const { register, handleSubmit, reset } = useForm<ContactProps>();
+  const { createContact } = useContacts() as {
+    createContact: createContactProps;
+  };
+
+  function onSubmit(data: ContactProps) {
+    createContact({ id: data.id, name: data.name });
+    reset();
+    closeModal();
+  }
+
   return (
     <div
       className="bg-white rounded-lg border-2 border-gray-200 p-6"
       style={{ width: "384px", height: "50%" }}>
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-xl font-semibold text-gray-800">
-          {" "}
-          New contact
-        </h2>
+        <h2 className="text-xl font-semibold text-gray-800">New contact</h2>
         <button
           onClick={closeModal}
           className="text-gray-500 hover:text-gray-700 text-xl font-bold">
@@ -20,6 +31,30 @@ export default function NewContactModal({
           x{" "}
         </button>
       </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mt-3">
+          <p className="text-left">Id</p>
+          <input
+            className="w-full h-10 border-1 border-gray-300 p-1"
+            {...register("id", { required: true })}
+          />
+        </div>
+
+        <div className="mt-3">
+          <p className="text-left">Name</p>
+          <input
+            className="w-full h-10 border-1 border-gray-300 p-1"
+            {...register("name", { required: true })}
+          />
+        </div>
+        <button
+          className="bg-blue-400 rounded mt-4 h-10 w-20 border-blue-500 border-2 text-white cursor-pointer hover:bg-blue-600"
+          type="submit">
+          {" "}
+          Create
+        </button>
+      </form>
     </div>
   );
 }
